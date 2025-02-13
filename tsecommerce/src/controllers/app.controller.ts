da7d8b594f59acec1b6ab7a32424894a209c35d1
Laxmi2708 <laxmi27patil@gmail.com>
2025-02-13 10:24:26 +0530

    commi4

---
+++ tsecommerce/src/controllers/app.controller.ts
@@ -0,0 +1,21 @@
+import angular from "angular";
+
+interface ICustomeScope extends angular.IScope {
+    vm: any;
+}
+export class myController {
+    static $inject = ['$scope'];
+    message:string;
+    users:Array<any>;
+    constructor(private $scope:ICustomeScope) {
+        this.message = 'Hello from myController';
+        this.users = [
+            {name: 'John', age: 25},
+            {name: 'Jane', age: 30},
+            {name: 'Jim', age: 35}
+        ];
+        $scope.vm = this;
+    }
+
+
+  }
